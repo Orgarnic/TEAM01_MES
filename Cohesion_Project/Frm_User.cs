66c871064d4189c9f057b8f2bc48888e436ccacf
KKMMSSS <kiminsic2008@naver.com>
2023-01-19 15:57:14 +0900

    사용자관리 커밋

---
+++ Cohesion_Project/Frm_User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cohesion_Project
+{
+    public partial class Frm_User : Frm_Base_2
+    {
+        public Frm_User()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_User_Load(object sender, EventArgs e)
+        {
+            DgvInit();
+        }
+        private void DgvInit()
+        {
+            DgvUtil.DgvInit(DgvUser);
+            DgvUtil.AddTextCol(DgvUser,  "사용자 코드", "USER_ID", 120, true, align: 1);
+            DgvUtil.AddTextCol(DgvUser, "사용자 이름", "USER_NAME", 120, true, align: 1);
+            DgvUtil.AddTextCol(DgvUser, "사용자 그룹", "USER_GROUP_CODE",  120, true, align: 1);
+            DgvUtil.AddTextCol(DgvUser, "암호 ", "USER_PASSWORD", 120, true, align: 1);
+            DgvUtil.AddTextCol(DgvUser, "부서 ", "USER_DEPARTMENT",  120, true, align: 1);
+            DgvUtil.AddTextCol(DgvUser, "생성 시간 ", "CREATE_TIME", 120, true, align: 2);
+            DgvUtil.AddTextCol(DgvUser, "사용자", "CREATE_USER_ID", 120, true, align: 1);
+            DgvUtil.AddTextCol(DgvUser, "수정 시간", "UPDATE_TIME",  120, true, align: 2);
+            DgvUtil.AddTextCol(DgvUser, "사용자 ", "UPDATE_USER_ID",  120, true, align: 1);
+        }
+    }
+
+}
---
+++ Cohesion_Project/Frm_UserGroup.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace Cohesion_Project
+{
+    partial class Frm_UserGroup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.UserInsert = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.BtnDelete = new System.Windows.Forms.Button();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.DgvUserGroup = new System.Windows.Forms.DataGridView();
+            this.pnlSearch.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel7.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvUserGroup)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlSearch
+            // 
+            this.pnlSearch.Size = new System.Drawing.Size(1275, 35);
+            // 
+            // label6
+            // 
+            this.label6.Text = "사용자 그룹 설정";
+            // 
+            // Txt_Search
+            // 
+            this.Txt_Search.Size = new System.Drawing.Size(801, 21);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.DgvUserGroup);
+            this.panel2.Size = new System.Drawing.Size(956, 412);
+            this.panel2.Controls.SetChildIndex(this.label3, 0);
+            this.panel2.Controls.SetChildIndex(this.DgvUserGroup, 0);
+            // 
+            // label3
+            // 
+            this.label3.Size = new System.Drawing.Size(954, 30);
+            this.label3.Text = "▶ 사용자 그룹조회";
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.FlatAppearance.BorderSize = 0;
+            this.btnSearch.Location = new System.Drawing.Point(817, 38);
+            // 
+            // panel4
+            // 
+            this.panel4.Size = new System.Drawing.Size(966, 72);
+            // 
+            // btnSearchCondition
+            // 
+            this.btnSearchCondition.FlatAppearance.BorderSize = 0;
+            this.btnSearchCondition.Location = new System.Drawing.Point(877, 38);
+            // 
+            // label7
+            // 
+            this.label7.Size = new System.Drawing.Size(964, 30);
+            // 
+            // panel5
+            // 
+            this.panel5.Size = new System.Drawing.Size(1275, 493);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Size = new System.Drawing.Size(1275, 493);
+            this.splitContainer1.SplitterDistance = 969;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.propertyGrid1);
+            this.panel7.Size = new System.Drawing.Size(299, 487);
+            this.panel7.Controls.SetChildIndex(this.label8, 0);
+            this.panel7.Controls.SetChildIndex(this.propertyGrid1, 0);
+            // 
+            // label8
+            // 
+            this.label8.Size = new System.Drawing.Size(297, 30);
+            // 
+            // Btn_Close
+            // 
+            this.Btn_Close.FlatAppearance.BorderSize = 0;
+            this.Btn_Close.Location = new System.Drawing.Point(1176, 538);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(29)))), ((int)(((byte)(33)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(838, 538);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "조회";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(29)))), ((int)(((byte)(33)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(1035, 538);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 35);
+            this.button2.TabIndex = 31;
+            this.button2.Text = "수정";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // UserInsert
+            // 
+            this.UserInsert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.UserInsert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(29)))), ((int)(((byte)(33)))));
+            this.UserInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold);
+            this.UserInsert.Location = new System.Drawing.Point(939, 538);
+            this.UserInsert.Name = "UserInsert";
+            this.UserInsert.Size = new System.Drawing.Size(95, 35);
+            this.UserInsert.TabIndex = 32;
+            this.UserInsert.Text = "생성";
+            this.UserInsert.UseVisualStyleBackColor = false;
+        
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(29)))), ((int)(((byte)(33)))));
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold);
+            this.button4.Location = new System.Drawing.Point(737, 538);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 35);
+            this.button4.TabIndex = 33;
+            this.button4.Text = "초기화";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // BtnDelete
+            // 
+            this.BtnDelete.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(29)))), ((int)(((byte)(33)))));
+            this.BtnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.999999F, System.Drawing.FontStyle.Bold);
+            this.BtnDelete.Location = new System.Drawing.Point(636, 540);
+            this.BtnDelete.Name = "BtnDelete";
+            this.BtnDelete.Size = new System.Drawing.Size(95, 35);
+            this.BtnDelete.TabIndex = 34;
+            this.BtnDelete.Text = "삭제";
+            this.BtnDelete.UseVisualStyleBackColor = false;
+            this.BtnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Location = new System.Drawing.Point(3, 39);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(233, 422);
+            this.propertyGrid1.TabIndex = 7;
+          
+            // 
+            // DgvUserGroup
+            // 
+            this.DgvUserGroup.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvUserGroup.Location = new System.Drawing.Point(174, 92);
+            this.DgvUserGroup.Name = "DgvUserGroup";
+            this.DgvUserGroup.RowTemplate.Height = 23;
+            this.DgvUserGroup.Size = new System.Drawing.Size(607, 227);
+            this.DgvUserGroup.TabIndex = 6;
+            // 
+            // Frm_UserGroup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1275, 576);
+            this.Controls.Add(this.BtnDelete);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.UserInsert);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Frm_UserGroup";
+            this.Text = "Frm_USER_GROUP";
+            this.Load += new System.EventHandler(this.Frm_UserGroup_Load);
+            this.Controls.SetChildIndex(this.Btn_Close, 0);
+            this.Controls.SetChildIndex(this.pnlSearch, 0);
+            this.Controls.SetChildIndex(this.panel5, 0);
+            this.Controls.SetChildIndex(this.button1, 0);
+            this.Controls.SetChildIndex(this.button2, 0);
+            this.Controls.SetChildIndex(this.UserInsert, 0);
+            this.Controls.SetChildIndex(this.button4, 0);
+            this.Controls.SetChildIndex(this.BtnDelete, 0);
+            this.pnlSearch.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.DgvUserGroup)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        protected System.Windows.Forms.Button button1;
+        protected System.Windows.Forms.Button button2;
+        protected System.Windows.Forms.Button UserInsert;
+        protected System.Windows.Forms.Button button4;
+        protected System.Windows.Forms.Button BtnDelete;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+        private System.Windows.Forms.DataGridView DgvUserGroup;
+    }
+}
