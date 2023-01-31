8b99df5deae1476faaa592e1999fbe65950d47d3
KKMMSSS <kiminsic2008@naver.com>
2023-01-31 16:26:07 +0900

    ㅁㅅ

---
+++ Cohesion_DTO/Function_User_Group_Rel_DTO.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Cohesion_DTO
+{
+    public class FUNCTION_USER_GROUP_REL_DTO
+    {
+        [Category("속성"), Description("USER_GROUP_CODE"), DisplayName("사용자 그룹 코드")]
+        public string USER_GROUP_CODE { get; set; }  //사용자 그룹 코드
+        [Category("속성"), Description("FUNCTION_CODE"), DisplayName("화면 기능 코드")]
+        public string FUNCTION_CODE { get; set; }    //화면 기능 코드
+        [Category("속성"), Description("CREATE_TIME"), DisplayName("생성 시간")]
+        public DateTime CREATE_TIME { get; set; }    //생성 시간
+        [Category("속성"), Description("CREATE_USER_ID"), DisplayName("생성 사용자")]
+        public string CREATE_USER_ID { get; set; }   //생성 사용자
+        [Category("속성"), Description("UPDATE_TIME"), DisplayName("변경 시간")]
+        public DateTime UPDATE_TIME { get; set; }    //변경 시간
+        [Category("속성"), Description("UPDATE_USER_ID"), DisplayName("변경 사용자")]
+        public string UPDATE_USER_ID { get; set; }  //변경 사용자
+        public string USER_GROUP_NAME { get; set; }  //사용자 그룹명
+        public string USER_GROUP_TYPE { get; set; }  //사용자 그룹 유형
+        public string FUNCTION_NAME { get; set; }  //사용자 그룹 유형
+    }
+
+    public class FUNCTION_USER_GROUP_REL_Condition_DTO
+    {
+        [Category("속성"), Description("USER_GROUP_CODE"), DisplayName("사용자 그룹 코드")]
+        public string USER_GROUP_CODE { get; set; }  //사용자 그룹 코드
+        [Category("속성"), Description("FUNCTION_CODE"), DisplayName("화면 기능 코드")]
+        public string FUNCTION_CODE { get; set; }  //화면 기능 코드
+    }
+}
---
+++ Cohesion_Project/Service/Srv_Function_User_Group_Rel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cohesion_DTO;
+using Cohesion_DAO;
+
+namespace Cohesion_Project
+{
+    public class Srv_Function_User_Group_Rel
+    {
+        public List<FUNCTION_USER_GROUP_REL_DTO> SelectFUG()
+        {
+            Function_User_Group_Rel_DAO dao = new Function_User_Group_Rel_DAO();
+            List<FUNCTION_USER_GROUP_REL_DTO> list = dao.SelectFUG();
+            dao.Dispose();
+
+            return list;
+        }
+
+        public List<FUNCTION_USER_GROUP_REL_DTO> SelectFUG1()
+        {
+            Function_User_Group_Rel_DAO dao = new Function_User_Group_Rel_DAO();
+            List<FUNCTION_USER_GROUP_REL_DTO> list = dao.SelectFUG1();
+            dao.Dispose();
+
+            return list;
+        }
+
+        public bool DeleteFUG(string functiocode)
+        {
+            Function_User_Group_Rel_DAO dao = new Function_User_Group_Rel_DAO();
+            bool result = dao.DeleteFUG(functiocode);
+            dao.Dispose();
+
+            return result;
+        }
+        //public bool InsertFUG(FUNCTION_USER_GROUP_REL_DTO dto)
+        //{
+        //    Function_User_Group_Rel_DAO dao = new Function_User_Group_Rel_DAO();
+        //    bool result = dao.InsertFUG(dto);
+        //    dao.Dispose();
+        //    return result;
+        //}
+
+        public bool UpdateFUG(FUNCTION_USER_GROUP_REL_DTO dto)
+        {
+            Function_User_Group_Rel_DAO dao = new Function_User_Group_Rel_DAO();
+            bool result = dao.UpdateFUG(dto);
+            dao.Dispose();
+            return result;
+        }
+
+        public List<FUNCTION_USER_GROUP_REL_DTO> SelectFUG2(FUNCTION_USER_GROUP_REL_Condition_DTO condtion)
+        {
+
+            Function_User_Group_Rel_DAO dao = new Function_User_Group_Rel_DAO();
+            List<FUNCTION_USER_GROUP_REL_DTO> list = dao.SelectFUG2(condtion);
+            dao.Dispose();
+
+            return list;
+        }
+
+    }
+}
