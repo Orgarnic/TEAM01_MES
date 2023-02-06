f7c897219a42e460a591f21bb926ae187c40a12d
KKMMSSS <kiminsic2008@naver.com>
2023-02-06 17:00:55 +0900

    45

--- Cohesion_Project/Frm_Function_User_Group_Rel.cs
+++ Cohesion_Project/Frm_Function_User_Group_Rel.cs
@@ -15,7 +15,7 @@ namespace Cohesion_Project
     {
         Srv_Function_User_Group_Rel SrvFUG = new Srv_Function_User_Group_Rel();
         List<FUNCTION_USER_GROUP_REL_DTO> FList;
-      
+        bool searchState = false;
         public Frm_Function_User_Group_Rel()
         {
             InitializeComponent();
@@ -25,37 +25,40 @@ namespace Cohesion_Project
         {
 
             DgvInit();
-            DataGridViewFill();
-            DataGridViewFill2();
-            PpgInit();
+            LoadData();
+            // DataGridViewFill();
 
+            // DataGridViewFill2();
+            // PpgInit();
 
+            PpgFUG.SelectedObject = new UserGoupCondition_DTO();
 
 
 
         }
 
-        //private void LoadData() // 2
-        //{
-        //    var list = SrvFUG.SelectFUGR(new FUNCTION_USER_GROUP_REL_DTO());
-        //    int seq = 1;
-        //    DgvF1.DataSource = list.Select((i) => new
-        //    {
-        //        USER_GROUP_CODE = i.USER_GROUP_CODE,
-        //        USER_GROUP_NAME = i.USER_GROUP_NAME,
-        //        USER_GROUP_TYPE = i.USER_GROUP_TYPE,
-        //        CREATE_TIME = i.CREATE_TIME,
-        //        CREATE_USER_ID = i.CREATE_USER_ID,
-        //        UPDATE_TIME = i.UPDATE_TIME,
-        //        UPDATE_USER_ID = i.UPDATE_USER_ID,
+        private void LoadData() // 2
+        {
+            var list = SrvFUG.SelectFUG1(new FUNCTION_USER_GROUP_REL_DTO());
+            int seq = 1;
+            DgvFUG.DataSource = list.Select((i) => new
+            {
+                USER_GROUP_CODE = i.USER_GROUP_CODE,
+                USER_GROUP_NAME = i.USER_GROUP_NAME,
+                USER_GROUP_TYPE = i.USER_GROUP_TYPE,
+                CREATE_TIME = i.CREATE_TIME,
+                CREATE_USER_ID = i.CREATE_USER_ID,
+                UPDATE_TIME = i.UPDATE_TIME,
+                UPDATE_USER_ID = i.UPDATE_USER_ID,
 
-        //        DISPLAY_SEQ = seq++
-        //    }).ToList();
-        //}
+                DISPLAY_SEQ = seq++
+            }).ToList();
+        }
 
         private void DgvInit()
         {
             DgvUtil.DgvInit(DgvFUG);
+            DgvUtil.AddTextCol(DgvFUG, "순번", "DISPLAY_SEQ", 180, readOnly: true, align: 1, frozen: true);
             DgvUtil.AddTextCol(DgvFUG, "사용자 그룹 코드", "USER_GROUP_CODE", 150, true, align: 1);
             DgvUtil.AddTextCol(DgvFUG, "사용자 그룹 명", "USER_GROUP_NAME", 120, true, align: 1);
             DgvUtil.AddTextCol(DgvFUG, "사용자 그룹 유형", "USER_GROUP_TYPE", 120, true, align: 1);
@@ -66,42 +69,29 @@ namespace Cohesion_Project
 
             DgvUtil.DgvInit(DgvF1);
             DgvUtil.AddTextCol(DgvF1, "순번", "DISPLAY_SEQ", 180, readOnly: true, align: 1, frozen: true);
-            DgvUtil.AddTextCol(DgvF1, "화면 기능 코드", "FUNCTION_NAME", 150, true, align: 1);
             DgvUtil.AddTextCol(DgvF1, "화면 기능명", "FUNCTION_CODE", 120, true, align: 1);
+            DgvUtil.AddTextCol(DgvF1, "화면 기능 코드", "FUNCTION_NAME", 150, true, align: 1);
 
 
             DgvUtil.DgvInit(DgvF2);
             DgvUtil.AddTextCol(DgvF2, "순번", "DISPLAY_SEQ", 180, readOnly: true, align: 1, frozen: true);
-            DgvUtil.AddTextCol(DgvF2, "화면 기능 코드", "FUNCTION_NAME", 150, true, align: 1);
             DgvUtil.AddTextCol(DgvF2, "화면 기능명", "FUNCTION_CODE", 120, true, align: 1);
+            DgvUtil.AddTextCol(DgvF2, "화면 기능 코드", "FUNCTION_NAME", 150, true, align: 1);
 
-   
-
-        }
-
-
-        private void PpgInit()
-        {
-            PpgFUG.PropertySort = PropertySort.Categorized;
-            PpgFUG.SelectedObject = new FUNCTION_USER_GROUP_REL_DTO();
-        }
-
-        private void DataGridViewFill()
-        {
-            FList = SrvFUG.SelectFUG();
-            DgvFUG.DataSource = FList;
-
-            //FList = SrvFUG.SelectFUG1();
-            //DgvF2.DataSource = FList;
-        }
-
-        private void DataGridViewFill2()
-        {
-
-
-            FList = SrvFUG.SelectFUG1();
+           // FList = SrvFUG.SelectFUG2();
             DgvF2.DataSource = FList;
+
+
+            FList =SrvFUG.SelectFUG2();
+            int seq = 1;
+            DgvF2.DataSource = FList.Select((i) => new FUNCTION_USER_GROUP_REL_DTO
+            {
+                FUNCTION_CODE = i.FUNCTION_CODE,
+                FUNCTION_NAME = i.FUNCTION_NAME,
+                DISPLAY_SEQ = seq++
+            }).ToList();
         }
+
 
         private void btnRight_Click(object sender, EventArgs e)
         {
@@ -167,9 +157,9 @@ namespace Cohesion_Project
 
             FUNCTION_USER_GROUP_REL_DTO dto = new FUNCTION_USER_GROUP_REL_DTO
             {
-                FUNCTION_CODE = DgvF2.Rows[idx].Cells[0].Value.ToString()
+                FUNCTION_CODE = DgvF2.Rows[idx].Cells[1].Value.ToString()
                ,
-                FUNCTION_NAME = DgvF2.Rows[idx].Cells[1].Value.ToString()
+                FUNCTION_NAME = DgvF2.Rows[idx].Cells[2].Value.ToString()
             };
 
             if (DgvF1.Rows.Count == 0)
@@ -181,7 +171,8 @@ namespace Cohesion_Project
                 var l1 = DgvF1.DataSource;
 
                 List<FUNCTION_USER_GROUP_REL_DTO> list = DgvF2.DataSource as List<FUNCTION_USER_GROUP_REL_DTO>;
-                list.RemoveAt(list.FindIndex((i) => i.FUNCTION_CODE.Equals(dto.FUNCTION_CODE, StringComparison.OrdinalIgnoreCase)));
+                int temp = list.FindIndex((i) => i.FUNCTION_CODE.Equals(dto.FUNCTION_CODE, StringComparison.OrdinalIgnoreCase));
+                list.RemoveAt(temp);
 
                 DgvF2.DataSource = list.Select((i) => new FUNCTION_USER_GROUP_REL_DTO
                 {
@@ -216,14 +207,15 @@ namespace Cohesion_Project
                 return;
             }
 
-            string operationCode = DgvFUG[1, e.RowIndex].Value.ToString();
-            var list = SrvFUG.SelectFUGR(operationCode);
+            string user_GROUP_CODE = DgvFUG[1, e.RowIndex].Value.ToString();
+            var list = SrvFUG.SelectFUGR(user_GROUP_CODE);
             int seq = 1;
             int seqI = 1;
             DgvF1.DataSource = list.Select((i) => new FUNCTION_USER_GROUP_REL_DTO
             {
-                FUNCTION_CODE = i.FUNCTION_CODE,
                 FUNCTION_NAME = i.FUNCTION_NAME,
+                FUNCTION_CODE = i.FUNCTION_CODE,
+
                 DISPLAY_SEQ = seq++
             }).ToList();
 
@@ -239,8 +231,8 @@ namespace Cohesion_Project
             if (DgvF1.Rows.Count != 0)
             {
                 btnAdd.Enabled = false;
-                btnLeft.Enabled = false;
-                btnRight.Enabled = false;
+                //btnLeft.Enabled = false;
+                //btnRight.Enabled = false;
                 btnUpdate.Enabled = true;
                 btnInsert.Enabled = true;
             }
@@ -251,6 +243,131 @@ namespace Cohesion_Project
                 btnUpdate.Enabled = false;
                 btnInsert.Enabled = false;
             }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+         
+            DgvF1.DataSource = null;
+            int seq = 1;
+            DgvF2.DataSource = FList.Select((i) => new FUNCTION_USER_GROUP_REL_DTO
+            {
+                FUNCTION_CODE = i.FUNCTION_CODE,
+                FUNCTION_NAME = i.FUNCTION_NAME,
+                DISPLAY_SEQ = seq++
+            }).ToList(); ;
+            //LoadData();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string operationCode = DgvFUG[1, DgvFUG.CurrentRow.Index].Value.ToString();
+            List<FUNCTION_USER_GROUP_REL_DTO> list = DgvF1.DataSource as List<FUNCTION_USER_GROUP_REL_DTO>;
+
+
+            bool result = SrvFUG.InsertFUG(operationCode, list);
+            if (result)
+            {
+                MboxUtil.MboxInfo("화면 기능 정보를 입력했습니다.");
+                DgvF1.DataSource = null;
+                int seq = 1;
+                DgvF2.DataSource = FList.Select((i) => new FUNCTION_USER_GROUP_REL_DTO
+                {
+                    FUNCTION_CODE = i.FUNCTION_CODE,
+                    FUNCTION_NAME = i.FUNCTION_NAME,
+                    DISPLAY_SEQ = seq++
+                }).ToList();
+                btnRefresh.PerformClick();
+                return;
+            }
+            else
+            {
+                MboxUtil.MboxError("화면 기능 정보 입력중 오류가 발생했습니다.");
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            btnLeft.Enabled = true;
+            btnRight.Enabled = true;
+            
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string operationCode = DgvFUG[1, DgvFUG.CurrentRow.Index].Value.ToString();
+            List<FUNCTION_USER_GROUP_REL_DTO> list = DgvF1.DataSource as List<FUNCTION_USER_GROUP_REL_DTO>;
+
+            if (DgvF1.Rows.Count == 0)
+            {
+                MboxUtil.MboxError("화념 기능 항목을 등록해주세요");
+                return;
+            }
+
+            bool result = SrvFUG.InsertFUG(operationCode, list);
+            if (result)
+            {
+                MboxUtil.MboxInfo("화면 기능 정보를 입력했습니다.");
+                DgvF1.DataSource = null;
+                int seq = 1;
+                DgvF2.DataSource = FList.Select((i) => new FUNCTION_USER_GROUP_REL_DTO
+                {
+                    FUNCTION_CODE = i.FUNCTION_CODE,
+                    FUNCTION_NAME = i.FUNCTION_NAME,
+                    DISPLAY_SEQ = seq++
+                }).ToList();
+                btnRefresh.PerformClick();
+                return;
+            }
+            else
+            {
+                MboxUtil.MboxError("화면 기능 정보 입력중 오류가 발생했습니다.");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearchCondition_Click(object sender, EventArgs e)
+        {
+            if (!searchState)
+            {
+                searchState = true;
+                PpgFUG.Enabled = true;
+                
+            }
+            else
+            {
+                searchState = false;
+                PpgFUG.SelectedObject = new UserGoupCondition_DTO();
+                PpgFUG.Enabled = false;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            UserGoupCondition_DTO dto = PpgFUG.SelectedObject as UserGoupCondition_DTO;
+             var list = SrvFUG.SelectF(dto);
+            int seq = 1;
+            DgvFUG.DataSource = list.Select((i) => new
+            {
+                USER_GROUP_CODE = i.USER_GROUP_CODE,
+                USER_GROUP_NAME = i.USER_GROUP_NAME,
+                USER_GROUP_TYPE = i.USER_GROUP_TYPE,
+                CREATE_TIME = i.CREATE_TIME,
+                CREATE_USER_ID = i.CREATE_USER_ID,
+                UPDATE_TIME = i.UPDATE_TIME,
+                UPDATE_USER_ID = i.UPDATE_USER_ID,
+
+                DISPLAY_SEQ = seq++
+            }).ToList();
         }
     }
 }
